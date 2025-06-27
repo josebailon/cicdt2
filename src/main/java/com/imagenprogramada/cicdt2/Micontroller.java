810a842b3da433cd92f6b998e446f71c50ed58fb
Jose Javier Bailón Ortiz <josebailon@gmail.com>
2025-06-27 21:46:34 +0200

    Inicio

---
+++ src/main/java/com/imagenprogramada/cicdt2/Micontroller.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.imagenprogramada.cicdt2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Jose
+ */
+@RestController
+public class Micontroller {
+    
+    @GetMapping("/hola")
+    public String hola(){
+        return "hola";
+    }
+    
+}
---
+++ src/test/java/com/imagenprogramada/cicdt2/Cicdt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagenprogramada.cicdt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicdt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
